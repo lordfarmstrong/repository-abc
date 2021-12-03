151741f60e95db15249bc23c27a7e5bb08b71cd5
lordfarmstrong <dastin1973@mail.ru>
2021-12-03 21:08:31 +0400

    first commit

---
+++ app/src/main/java/com/example/mazzred3/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mazzred3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fun main() {
+            val list = mutableListOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+            for (i in 1 until list.size step 2) {
+                println(list[i])
+            }
+        }
+    }
+}
